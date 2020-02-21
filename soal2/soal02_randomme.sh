e344fc5228ff843306a503384d85cfc5d7c02f4a
abdurrohman100 <abdrohman0100@gmail.com>
2020-02-21 15:35:33 +0700

    Balajar Git

--- soal2/soal02_randomme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-fileOut="$1.txt"
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 27 >> $fileOut
-cat /dev/urandom | tr -dc '0-9' | head -c 1 >> $fileOut
-#echo $fileOut $(date +%H) >> logs.log
